90bf270cecebc61f86f5f25836367cf730990ad8
CommanderPho <PhoHale@gmail.com>
2021-01-29 17:49:28 -0500

    initial

--- PhoDibaAnalyze.m
+++ PhoDibaAnalyze.m
@@ -17,16 +17,47 @@ processing_config.active_expt.name = 'RoyMaze1';
 processing_config.active_expt.spikes_list = source_data.spikes.(processing_config.active_expt.name);
 processing_config.active_expt.behavior_list = source_data.behavior.(processing_config.active_expt.name).list;
 
+
+behavioral_state_names = source_data.behavior.(processing_config.active_expt.name).name;
+
+behavioral_epoch_names = {'pre_sleep', 'track', 'post_sleep'};
+
+for i = 1:length(behavioral_epoch_names)
+    curr_epoch_name = behavioral_epoch_names{i};
+    curr_epoch_start_stop = source_data.behavior.(processing_config.active_expt.name).time(i,:);
+    
+    % microseconds (10^6): 1000000
+    % nanoseconds (10^9): 1000000000
+    
+    curr_epoch_duration = (curr_epoch_start_stop(2) - curr_epoch_start_stop(1)) / 1000000;
+    disp(curr_epoch_duration)
+    behavioral_epochs.(curr_epoch_name) = [curr_epoch_start_stop curr_epoch_duration];
+end
+
 % curr_activity_table = array2table(processing_config.active_expt.behavior_list, ...
 %         'VariableNames',{'epoch_start_time', 'epoch_end_time', 'type'});
     
-curr_activity_table = table(datetime(processing_config.active_expt.behavior_list(:,1),'ConvertFrom','epochtime','TicksPerSecond',1000),  ...
-    datetime(processing_config.active_expt.behavior_list(:,2),'ConvertFrom','epochtime','TicksPerSecond',1000),  ...
-    processing_config.active_expt.behavior_list(:,3),  ...
+curr_activity_table = table(convert_date(processing_config.active_expt.behavior_list(:,1)),  ...
+    convert_date(processing_config.active_expt.behavior_list(:,2)),  ...
+    categorical(processing_config.active_expt.behavior_list(:,3), [1:length(behavioral_state_names)], behavioral_state_names),  ...
     'VariableNames',{'epoch_start_time', 'epoch_end_time', 'type'});
     
+TimeWindows
+    FileLoaderOpenEphys
+function [date_out] = convert_date(aDateTimestamp)
+%% Raw Timestamps Mode:
+return_raw_timestamps = true;
 
-    
+if return_raw_timestamps
+   date_out = aDateTimestamp;
+else
+    %     date_out = datetime(aDateTimestamp,'ConvertFrom','epochtime','TicksPerSecond',1000);
+    date_out = datetime(aDateTimestamp,'ConvertFrom','epochtime','Epoch','2000-01-01','TicksPerSecond', 1000000); % Prety close
+    %     date_out = datetime(aDateTimestamp,'ConvertFrom','datenum');
+end % end if
+end % end function convert_date
+
+
 %     table(indexArray', final_data_explorer_obj.uniqueComps, final_data_explorer_obj.cellROIIndex_mapper.compIDsArray, final_data_explorer_obj.multiSessionCellRoi_CompListIndicies,...
 % 		manualRoiFilteringResults.final_is_Excluded, manualRoiFilteringResults.final_quality_of_tuning, ...
 % 'datetime'
