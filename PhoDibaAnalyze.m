b09c318323339dae6300fcd06d8c71ab8cf2f4fa
CommanderPho <PhoHale@gmail.com>
2021-01-29 17:49:40 -0500

    Improved time import format

--- PhoDibaAnalyze.m
+++ PhoDibaAnalyze.m
@@ -7,6 +7,10 @@ data_config.source_data_prefix = 'Hiro_Datasets';
 data_config.source_root_path = fullfile(data_config.root_parent_path, data_config.source_data_prefix);
 
 
+% microseconds (10^6): 1000000
+% nanoseconds (10^9): 1000000000
+conversion_factor = (10^6);
+
 % ,'y'
 source_data.spikes = load(fullfile(data_config.source_root_path,'wake-spikes.mat'), 'spikes').spikes;
 source_data.behavior = load(fullfile(data_config.source_root_path, 'wake-behavior.mat'), 'behavior').behavior;
@@ -22,37 +26,56 @@ behavioral_state_names = source_data.behavior.(processing_config.active_expt.nam
 
 behavioral_epoch_names = {'pre_sleep', 'track', 'post_sleep'};
 
+active_processing.earliest_start_timestamp = Inf; % Set the initial timestamp to be infinity, so that any timestamp it's compared to will be less than it
+
 for i = 1:length(behavioral_epoch_names)
-    curr_epoch_name = behavioral_epoch_names{i};
-    curr_epoch_start_stop = source_data.behavior.(processing_config.active_expt.name).time(i,:);
+    temp.curr_epoch_name = behavioral_epoch_names{i};
+    temp.curr_epoch_start_stop = source_data.behavior.(processing_config.active_expt.name).time(i,:);
     
     % microseconds (10^6): 1000000
     % nanoseconds (10^9): 1000000000
-    
-    curr_epoch_duration = (curr_epoch_start_stop(2) - curr_epoch_start_stop(1)) / 1000000;
-    disp(curr_epoch_duration)
-    behavioral_epochs.(curr_epoch_name) = [curr_epoch_start_stop curr_epoch_duration];
+    active_processing.earliest_start_timestamp = min(active_processing.earliest_start_timestamp, (temp.curr_epoch_start_stop(1) / conversion_factor));
+    temp.curr_epoch_duration = ((temp.curr_epoch_start_stop(2) - temp.curr_epoch_start_stop(1)) / conversion_factor);
+    disp(temp.curr_epoch_duration)
+    active_processing.behavioral_epochs.(temp.curr_epoch_name) = [temp.curr_epoch_start_stop temp.curr_epoch_duration];
 end
 
+
+fprintf('earliest recording timestamp is %d\n', active_processing.earliest_start_timestamp)
 % curr_activity_table = array2table(processing_config.active_expt.behavior_list, ...
 %         'VariableNames',{'epoch_start_time', 'epoch_end_time', 'type'});
     
-curr_activity_table = table(convert_date(processing_config.active_expt.behavior_list(:,1)),  ...
-    convert_date(processing_config.active_expt.behavior_list(:,2)),  ...
-    categorical(processing_config.active_expt.behavior_list(:,3), [1:length(behavioral_state_names)], behavioral_state_names),  ...
-    'VariableNames',{'epoch_start_time', 'epoch_end_time', 'type'});
-    
-TimeWindows
-    FileLoaderOpenEphys
-function [date_out] = convert_date(aDateTimestamp)
-%% Raw Timestamps Mode:
-return_raw_timestamps = true;
 
+temp.durations = ((processing_config.active_expt.behavior_list(:,2) - processing_config.active_expt.behavior_list(:,1)) ./ conversion_factor);
+
+
+% Datetime based:
+curr_activity_table = table(((processing_config.active_expt.behavior_list(:,1) ./ conversion_factor) - active_processing.earliest_start_timestamp),  ...
+    ((processing_config.active_expt.behavior_list(:,2) ./ conversion_factor) - active_processing.earliest_start_timestamp),  ...
+    temp.durations, ...
+    categorical(processing_config.active_expt.behavior_list(:,3), [1:length(behavioral_state_names)], behavioral_state_names),  ...
+    'VariableNames',{'epoch_start_seconds', 'epoch_end_seconds', 'duration', 'type'});
+
+
+% % Datetime based:
+% curr_activity_table = table(convert_date(processing_config.active_expt.behavior_list(:,1), conversion_factor),  ...
+%     convert_date(processing_config.active_expt.behavior_list(:,2), conversion_factor),  ...
+%     temp.durations, ...
+%     categorical(processing_config.active_expt.behavior_list(:,3), [1:length(behavioral_state_names)], behavioral_state_names),  ...
+%     'VariableNames',{'epoch_start_time', 'epoch_end_time', 'duration', 'type'});
+    
+% TimeWindows
+%     FileLoaderOpenEphys
+function [date_out] = convert_date(aDateTimestamp, conversion_factor)
+%% Raw Timestamps Mode:
+return_raw_timestamps = false;
+    timestamps_seconds = aDateTimestamp ./ conversion_factor;
 if return_raw_timestamps
-   date_out = aDateTimestamp;
+   date_out = timestamps_seconds
 else
     %     date_out = datetime(aDateTimestamp,'ConvertFrom','epochtime','TicksPerSecond',1000);
-    date_out = datetime(aDateTimestamp,'ConvertFrom','epochtime','Epoch','2000-01-01','TicksPerSecond', 1000000); % Prety close
+%     date_out = datetime(timestamps_seconds,'ConvertFrom','epochtime','Epoch','2000-01-01','TicksPerSecond', conversion_factor); % Prety close
+    date_out = datetime(timestamps_seconds,'ConvertFrom','epochtime');
     %     date_out = datetime(aDateTimestamp,'ConvertFrom','datenum');
 end % end if
 end % end function convert_date
