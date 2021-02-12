f57a3ff2667818b92de8d00d8f3926dc3a06f2de
CommanderPho <PhoHale@gmail.com>
2021-02-11 19:14:43 -0500

    minor

---
+++ README.m
@@ -0,0 +1,21 @@
+    %% active_processing: pre-processed results
+        %% behavioral_state_names: TODO
+        %% earliest_start_timestamp: TODO
+        %% behavioral_epochs: TODO
+        %% behavioral_periods_table: TODO
+        %% spikes: TODO
+        %% processed_array: TODO 
+
+    %% active_results: depend on the bin size
+        %% all: TODO
+        %% by_epoch: TODO
+        %% aggregates: TODO
+        %% by_state: TODO 
+
+    %% general_results: results that are independent of binning resolution
+        %% indicies: TODO
+        %% GroupedByState: TODO
+        %% flattened_across_all_units: TODO
+        %% edges: TODO
+        %% per_behavioral_state_period: TODO
+        %% counts: TODO 
