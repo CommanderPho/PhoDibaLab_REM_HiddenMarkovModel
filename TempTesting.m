0a94365f3a14f1646efd257501fab168538c5690
CommanderPho <PhoHale@gmail.com>
2021-02-18 17:41:51 -0500

    minor

--- TempTesting.m
+++ TempTesting.m
@@ -6,16 +6,16 @@ addpath(genpath('libraries/buzcode/'));
 % clear all;
 
 temp.possibleRootPaths = {'/Volumes/iNeo/Rotation_3_Kamran Diba Lab/DataProcessingProject/Hiro_Datasets', ...
-	'/Users/pho/Dropbox/Classes/Spring 2020/PIBS 600 - Rotations/Rotation_3_Kamran Diba Lab/DataProcessingProject/Hiro_Datasets'};
+	'/Users/pho/Dropbox/Classes/Spring 2021/PIBS 600 - Rotations/Rotation_3_Kamran Diba Lab/DataProcessingProject/Hiro_Datasets'};
 
 temp.possibleExperiments = {'RoyMaze1',	'RoyMaze2',	'RoyMaze3',	'TedMaze1',	'TedMaze2',	'TedMaze3',	'KevinMaze1'};
 
 % active_expt_name = 'TedMaze3';
-active_expt_name = 'KevinMaze1';
-% auxillary pink rMBP:
-active_root_path = temp.possibleRootPaths{1};
-% % main rMBP:
-% active_root_path = temp.possibleRootPaths{2};
+active_expt_name = 'RoyMaze3';
+% % auxillary pink rMBP:
+% active_root_path = temp.possibleRootPaths{1};
+% main rMBP:
+active_root_path = temp.possibleRootPaths{2};
 
 active_step_sizes = {0.1, 1.0};
 fprintf('fnRunPipeline starting for active_expt_name: %s...\n', active_expt_name);
