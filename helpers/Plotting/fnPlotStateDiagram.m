2bb014c5b267512aa0c67e061344421602296834
CommanderPho <PhoHale@gmail.com>
2021-02-22 13:44:58 -0500

    added behavioral_epoch vs. behavioral_state support

--- helpers/Plotting/fnPlotStateDiagram.m
+++ helpers/Plotting/fnPlotStateDiagram.m
@@ -3,38 +3,41 @@ function [ax] = fnPlotStateDiagram(active_processing, plottingOptions)
 %   Detailed explanation goes here
 
 
-if ~exist('plottingOptions','var')
-%     plottingOptions.vertical_state_mode = 'stacked';
-    plottingOptions.vertical_state_mode = 'combined';
-    
-    
-end
+    if ~exist('plottingOptions','var')
+    %     plottingOptions.vertical_state_mode = 'stacked';
+        plottingOptions.vertical_state_mode = 'combined';
+
+        plottingOptions.plot_variable = 'behavioral_epoch';
+    %     plottingOptions.plot_variable = 'behavioral_state';
+
+        % 
+    end
 
 
-%     state_names={'wake','nrem','rem'}; 
-
-    
-    % Table:
-%     state_changes = across_experiment_results{1, 1}.active_processing.behavioral_periods_table.epoch_start_seconds;
-    
-%     state_changes=[0;cumsum(rand(10,1)*10)];
-%     states=[state_changes(1:end-1),state_changes(2:end),mod(cumsum(randi(2,10,1)),3)+1];
-
-
-    % behavioral_epoch
-%     state_names = across_experiment_results{1, 1}.active_processing.definitions.behavioral_epoch.classNames;
-
-    % behavioral_state
-    state_names = active_processing.definitions.behavioral_state.classNames;
-    states = [active_processing.behavioral_periods_table.epoch_start_seconds, ...
-        active_processing.behavioral_periods_table.epoch_end_seconds, ...
-        double(active_processing.behavioral_periods_table.type)];
-    color_state=[0.5, 0.5, 1.0
-           0.7, 0.7, 1.0
-           1.0, 0.7, 0.7
-           1.0, 0.0, 0.0];
-       
-    
+    if strcmpi(plottingOptions.plot_variable, 'behavioral_epoch')
+        % behavioral_epoch
+        state_names = active_processing.definitions.behavioral_epoch.classNames;
+        states = [active_processing.behavioral_periods_table.epoch_start_seconds, ...
+            active_processing.behavioral_periods_table.epoch_end_seconds, ...
+            double(active_processing.behavioral_periods_table.behavioral_epoch)];
+        color_state=[0.0, 0.5, 0.0
+               0.2, 1.0, 0.2
+               0.0, 0.2, 0.0];
+           
+    elseif strcmpi(plottingOptions.plot_variable, 'behavioral_state')
+        % behavioral_state
+        state_names = active_processing.definitions.behavioral_state.classNames;
+        states = [active_processing.behavioral_periods_table.epoch_start_seconds, ...
+            active_processing.behavioral_periods_table.epoch_end_seconds, ...
+            double(active_processing.behavioral_periods_table.type)];
+        color_state=[0.5, 0.5, 1.0
+               0.7, 0.7, 1.0
+               1.0, 0.7, 0.7
+               1.0, 0.0, 0.0];
+    else
+        error('invalid plottingOptions.plot_variable');
+    end
+            
     t = 0:1:states(end, 2);
     
     for s_idx = 1:size(states,1)
