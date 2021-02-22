729c77fc9c02215bf79acd4a9c1f18514daca2ea
CommanderPho <PhoHale@gmail.com>
2021-02-22 13:30:53 -0500

    Added support for a combined vertical representation (partition-like)

--- helpers/Plotting/fnPlotStateDiagram.m
+++ helpers/Plotting/fnPlotStateDiagram.m
@@ -1,6 +1,14 @@
-function [ax] = fnPlotStateDiagram(active_processing)
+function [ax] = fnPlotStateDiagram(active_processing, plottingOptions)
 %fnPlotStateDiagram Plots a behavioral-state indicator as a function of time
 %   Detailed explanation goes here
+
+
+if ~exist('plottingOptions','var')
+%     plottingOptions.vertical_state_mode = 'stacked';
+    plottingOptions.vertical_state_mode = 'combined';
+    
+    
+end
 
 
 %     state_names={'wake','nrem','rem'}; 
@@ -11,8 +19,6 @@ function [ax] = fnPlotStateDiagram(active_processing)
     
 %     state_changes=[0;cumsum(rand(10,1)*10)];
 %     states=[state_changes(1:end-1),state_changes(2:end),mod(cumsum(randi(2,10,1)),3)+1];
-
-
 
 
     % behavioral_epoch
@@ -31,17 +37,34 @@ function [ax] = fnPlotStateDiagram(active_processing)
     
     t = 0:1:states(end, 2);
     
-    for s_idx=1:size(states,1)
-        rectangle('Position',[states(s_idx,1),states(s_idx,3)-0.5,diff(states(s_idx,1:2)),1],...
+    for s_idx = 1:size(states,1)
+        if strcmpi(plottingOptions.vertical_state_mode, 'stacked')
+            curr_s_y = states(s_idx,3)-0.5;
+        else
+            % in combined mode they all have the same y-position.
+            curr_s_y = 1.0-0.5;
+        end
+        rectangle('Position',[states(s_idx,1), curr_s_y, diff(states(s_idx,1:2)),1],...
                     'LineStyle','none','facecolor',color_state(states(s_idx,3),:))
     end
 
     ax=gca;
-    ax.YTick=1:length(state_names);
-    ax.YTickLabel=state_names;
+    
+    if strcmpi(plottingOptions.vertical_state_mode, 'stacked')
+        ax.YTick=1:length(state_names);
+        ax.YTickLabel=state_names;
+        ax.YLim=0.5+[0,length(state_names)];
+    else
+        % combined mode:
+        ax.YLim=0.5+[0,1];
+        ax.YTickLabel='state';
+        
+    end
+        
     ax.YDir='reverse';
+    
+  
     ax.XLim=t([1,end]);
-    ax.YLim=0.5+[0,length(state_names)];
     ax.XAxis.Visible = 'off';
 
    
