e13d4bf455ddd2b5ea2c851e58e5b15658e8f4a4
CommanderPho <PhoHale@gmail.com>
2021-02-25 16:38:14 -0500

    Enabled toggling of behavioral_state, behavioral_epoch

--- helpers/Plotting/Utilities/fnPlotHelper_AddStateMapSubplot.m
+++ helpers/Plotting/Utilities/fnPlotHelper_AddStateMapSubplot.m
@@ -13,14 +13,15 @@ function [state_ax, epoch_ax] = fnPlotHelper_AddStateMapSubplot(active_processin
          plottingOptions = struct();
      end
     
-     common_statemapPlottingOptions = fnAddDefaultOptionalArgs({'x_axis', 'orientation', 'vertical_state_mode'}, ...
-        {'index', 'vertical', 'combined'}, ...
+     common_statemapPlottingOptions = fnAddDefaultOptionalArgs({'x_axis', 'orientation', 'vertical_state_mode', 'include_state', 'include_epoch'}, ...
+        {'index', 'vertical', 'combined', true, true}, ...
         plottingOptions);
     
     
     
     % Resize current plots:
-    temp.curr_heatmap_pos = curr_axis.Position;
+    % temp.curr_heatmap_pos = curr_axis.Position;
+
     temp.updated_heatmap_pos = curr_axis.Position;
     temp.updated_heatmap_pos(1) = 0.1; % Move left edge (x) to 0.1 in relative coords.
 %     temp.updated_heatmap_pos(3) = temp.curr_heatmap_pos(3) * 0.9; % Set to 90% of original width
@@ -28,33 +29,39 @@ function [state_ax, epoch_ax] = fnPlotHelper_AddStateMapSubplot(active_processin
     
     
     %% Add the behavioral period map:
-    state_statemapPlottingOptions.orientation = common_statemapPlottingOptions.orientation;
-    state_statemapPlottingOptions.vertical_state_mode = common_statemapPlottingOptions.vertical_state_mode;
-    state_statemapPlottingOptions.plot_variable = 'behavioral_state';
-    state_statemapPlottingOptions.x_axis = common_statemapPlottingOptions.x_axis;
-    
-    
-    
-    temp.statemap_pos = temp.updated_heatmap_pos;
-    temp.statemap_pos(1) = 0.9;
-    temp.statemap_pos(1) = temp.updated_heatmap_pos(1) + temp.updated_heatmap_pos(3);
-    temp.statemap_pos(3) = 0.05;
-    
-    subplot('Position', temp.statemap_pos);
-    [state_ax] = fnPlotStateDiagram(active_processing, state_statemapPlottingOptions);
-
+    if common_statemapPlottingOptions.include_state
+		state_statemapPlottingOptions.orientation = common_statemapPlottingOptions.orientation;
+		state_statemapPlottingOptions.vertical_state_mode = common_statemapPlottingOptions.vertical_state_mode;
+		state_statemapPlottingOptions.plot_variable = 'behavioral_state';
+		state_statemapPlottingOptions.x_axis = common_statemapPlottingOptions.x_axis;
+		
+		temp.statemap_pos = temp.updated_heatmap_pos;
+		temp.statemap_pos(1) = 0.9;
+		temp.statemap_pos(1) = temp.updated_heatmap_pos(1) + temp.updated_heatmap_pos(3);
+		temp.statemap_pos(3) = 0.05;
+		
+		subplot('Position', temp.statemap_pos);
+		[state_ax] = fnPlotStateDiagram(active_processing, state_statemapPlottingOptions);
+	else
+		state_ax = [];
+	end
     
     %% Plot Epoch Position map
-    epoch_statemapPlottingOptions.orientation = common_statemapPlottingOptions.orientation;
-    epoch_statemapPlottingOptions.vertical_state_mode = common_statemapPlottingOptions.vertical_state_mode;
-    epoch_statemapPlottingOptions.plot_variable = 'behavioral_epoch';
-    epoch_statemapPlottingOptions.x_axis = common_statemapPlottingOptions.x_axis;
-    
-    temp.epoch_statemap_pos = temp.statemap_pos;
-    temp.epoch_statemap_pos(1) = temp.statemap_pos(1) + temp.statemap_pos(3);
-    temp.epoch_statemap_pos(3) = 0.025;
-    
-    subplot('Position', temp.epoch_statemap_pos);
-%     [epoch_ax] = fnPlotStateDiagram(active_processing, epoch_statemapPlottingOptions);
+	 if common_statemapPlottingOptions.include_epoch
+		epoch_statemapPlottingOptions.orientation = common_statemapPlottingOptions.orientation;
+		epoch_statemapPlottingOptions.vertical_state_mode = common_statemapPlottingOptions.vertical_state_mode;
+		epoch_statemapPlottingOptions.plot_variable = 'behavioral_epoch';
+		epoch_statemapPlottingOptions.x_axis = common_statemapPlottingOptions.x_axis;
+		
+		temp.epoch_statemap_pos = temp.statemap_pos;
+		temp.epoch_statemap_pos(1) = temp.statemap_pos(1) + temp.statemap_pos(3);
+		temp.epoch_statemap_pos(3) = 0.025;
+		
+		subplot('Position', temp.epoch_statemap_pos);
+		[epoch_ax] = fnPlotStateDiagram(active_processing, epoch_statemapPlottingOptions);
+	else
+		epoch_ax = [];
+	end
+
 end
 
