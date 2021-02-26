96309d328e4152c49c5788ac51250ba1f9215585
CommanderPho <PhoHale@gmail.com>
2021-02-26 14:41:32 -0500

    line positioning works great

--- helpers/Plotting/InteractiveRasterPlot/phoSelectionAnnotations.m
+++ helpers/Plotting/InteractiveRasterPlot/phoSelectionAnnotations.m
@@ -51,9 +51,9 @@ function phoSelectionAnnotations(hPlot)
     handles.hLines = hLines;
     
     % Pack Gui Data:
-    data.handles = handles;
-    
-    guidata(gcf, data)
+    dataPack.handles = handles;
+    dataPack.plottingOptions.lineVisibleColor = [0.95 0.95 0.95];
+    guidata(gcf, dataPack)
 
 end
 
@@ -86,11 +86,11 @@ function mouseDownCallback_phoSelectionAnnotations(src, eventdata)
         if left
             % the left mouse button is double clicked, so place the first mark
             % line to the new pointer possition 
-            set(handles.hLines(1), 'XData', [ppos(1,1), ppos(1,1)], 'Color', 'k')   
+            set(handles.hLines(1), 'XData', [ppos(1,1), ppos(1,1)], 'Color', dataPack.plottingOptions.lineVisibleColor)   
         else
             % the right mouse button is double clicked, so place the first mark
             % line to the new pointer possition 
-            set(handles.hLines(2), 'XData', [ppos(1,1), ppos(1,1)], 'Color', 'k') 
+            set(handles.hLines(2), 'XData', [ppos(1,1), ppos(1,1)], 'Color', dataPack.plottingOptions.lineVisibleColor) 
         end
 
         % updata the figure
@@ -241,7 +241,7 @@ function buildLines_phoSelectionAnnotations(xPositions, associatedData, hFig)
     handles = dataPack.handles;
     
     numUpdatedLines = length(xPositions);
-    
+        
     if ~isfield(handles, 'hLines')
        handles.hLines = gobjects(numUpdatedLines, 1); % Initialize new objects array
     end
@@ -260,12 +260,20 @@ function buildLines_phoSelectionAnnotations(xPositions, associatedData, hFig)
             handles.hLines(j) = line([axlms(1)+xlen/3, axlms(1)+xlen/3], [axlms(3), axlms(4)],...
                       'LineStyle', '-.', 'LineWidth', 2, 'Color', 'none'); 
         end
-    
+        
+    elseif numNewLinesNeeded < 0
+        % We need to remove lines:
+        for j = numExtantLines+1:numUpdatedLines
+            %% Creates Invisible Lines to begin with ('Color', 'none') which are repositioned when they're made visible.
+%             set(handles.hLines(j), 'Color', 'none');
+            delete(handles.hLines(j));
+        end
+        
     end
     
     % Loop through all lines to position them, including extant ones.
     for i = 1:length(xPositions)
-        set(handles.hLines(i), 'XData', [xPositions(i), xPositions(i)], 'Color', 'k')       
+        set(handles.hLines(i), 'XData', [xPositions(i), xPositions(i)], 'Color', dataPack.plottingOptions.lineVisibleColor)       
     end
     
     % Pack Gui Data:
@@ -275,100 +283,3 @@ function buildLines_phoSelectionAnnotations(xPositions, associatedData, hFig)
 
 end
     
-
-% function nested_buildNewLine(xPos, associatedDataItem)
-%    %% Creates Invisible Lines to begin with ('Color', 'none') which are repositioned when they're made visible.
-%     % section start-line creation
-%     hLine1 = line([axlms(1)+xlen/3, axlms(1)+xlen/3], [axlms(3), axlms(4)],...
-%                   'LineStyle', '-.', 'LineWidth', 2, 'Color', 'none'); 
-% 
-% end
-    
-%     
-% %% OLD:
-% function createlines(src, eventdata)
-% 
-%     % Note: Bellow, a special code is developed in order to determine which  
-%     % of the two mouse button is double clicked. The Matlab do not support  
-%     % such feature as an build-in functionality.
-% 
-%     % retrive the Handles Structure
-%     handles = guidata(gcf);
-% 
-%     % make the variable "left" persistent - its value must 
-%     % retained in memory between calls of the function
-%     persistent left
-% 
-%     % determine which button is double clicked - the left or the right
-%     if strcmp(get(src, 'SelectionType'), 'normal')
-%         left = true;
-%         pause(0.5) 
-%     elseif strcmp(get(src, 'SelectionType'), 'alt')
-%         left = false;
-%         pause(0.5)   
-%     elseif strcmp(get(src, 'SelectionType'), 'open')
-%         % get the current pointer possition
-%         ppos = get(gca, 'CurrentPoint');
-%         if left
-%             % the left mouse button is double clicked, so place the first mark
-%             % line to the new pointer possition 
-%             set(handles.hLines(1), 'XData', [ppos(1,1), ppos(1,1)], 'Color', 'k')   
-%         else
-%             % the right mouse button is double clicked, so place the first mark
-%             % line to the new pointer possition 
-%             set(handles.hLines(2), 'XData', [ppos(1,1), ppos(1,1)], 'Color', 'k') 
-%         end
-% 
-%         % updata the figure
-%         drawnow
-%     end
-% 
-%     % update the Handles Structure
-%     guidata(gcf, handles)
-% 
-% end
-% 
-% 
-% function savedata(src, eventdata)
-% 
-%     % retrive the Handles Structure
-%     handles = guidata(gcf);
-% 
-%     switch eventdata.Key
-% 
-%         case 'return'
-%             % determine the mark lines' abscissa values
-%             secstart = get(handles.hLines(1), 'XData');
-%             secend = get(handles.hLines(2), 'XData');
-%             lpos = sort([secstart(1), secend(1)]);
-% 
-%             % determine the selected data
-%             xdata = get(handles.hPlot, 'XData');
-%             ydata = get(handles.hPlot, 'YData');
-%             xsel = xdata(xdata >= lpos(1) & xdata <= lpos(2));
-%             ysel = ydata(xdata >= lpos(1) & xdata <= lpos(2));
-%             data = [xsel(:), ysel(:)]; %#ok<NASGU>
-% 
-%             %% TODO: Enable saving to workspace variable instead of out to file.
-%             % organize a save dialog menu
-%             [filename, pathname] = uiputfile({'*.mat', 'MAT-file (*.mat)'}, 'Save as...');
-%             if filename == 0, return, end
-% 
-%             % try to save the selected data in a .mat file
-%             try
-%                 save([pathname filename], 'data')
-%             catch
-%                 errordlg('An error was emerge!', 'Error', 'modal');
-%                 return
-%             end
-% 
-%         case 'end'
-%             % end of the selsec function operation
-%             set(gcf, 'WindowButtonDownFcn', '') % TODO: Do I need to restore the default one, or the one set by scrollplot(...)?
-%             set(gcf, 'KeyPressFcn', '')
-%             delete(handles.hLines)
-% 
-%     end
-% 
-% end
-        
