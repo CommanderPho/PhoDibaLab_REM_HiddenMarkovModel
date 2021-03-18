10eca2fcb3c22a3ad3209d91a5a1e91a0168df48
CommanderPho <PhoHale@gmail.com>
2021-03-17 20:15:44 -0400

    Added ability to export single experiment results

--- helpers/LoadFromMultiExperimentResults.m
+++ helpers/LoadFromMultiExperimentResults.m
@@ -3,6 +3,10 @@
 
 % multi_experiment_config.active_experiment_id: the index of the active experiment to load
 multi_experiment_config.active_experiment_id = 1;
+
+multi_experiment_config.should_save_individual_result = true;
+
+multi_experiment_config.active_experiment_name = active_experiment_names{multi_experiment_config.active_experiment_id};
 % current_binning_index = 2;
 % active_binning_resolution = processing_config.step_sizes{current_binning_index};
 % temp.curr_timestamps = timesteps_array{current_binning_index};
@@ -27,6 +31,26 @@ else
         error('active_processing will not be overwritten by this function!');
         fprintf('active_processing already exists in workspace. Using extant data.\n');
     end
+
+end
+
+
+%% Save out active experiment as separate .mat file:
+if multi_experiment_config.should_save_individual_result
+        
+    %% Set the path to the combined across_experiment file:
+    % data_config.output.all_expts_combined_parent_path = '/Users/pho/Dropbox/Classes/Spring 2020/PIBS 600 - Rotations/Rotation_3_Kamran Diba Lab/DataProcessingProject/Hiro_Datasets';
+    data_config.output.all_expts_combined_parent_path = '/Volumes/iNeo/Rotation_3_Kamran Diba Lab/DataProcessingProject/Hiro_Datasets/Results';
+    data_config.output.active_expt_combined_results_file_name = sprintf('PhoResults_Expt%d_%s.mat', multi_experiment_config.active_experiment_id, multi_experiment_config.active_experiment_name);
+    data_config.output.active_expt_combined_results_file_path = fullfile(data_config.output.all_expts_combined_parent_path, data_config.output.active_expt_combined_results_file_name);
+
+    fprintf('writing out individual active expt to %s...\n', data_config.output.active_expt_combined_results_file_path);
+    if ~exist(data_config.output.active_expt_combined_results_file_path,'file')
+        save(data_config.output.active_expt_combined_results_file_path, 'active_processing', 'processing_config', 'num_of_electrodes', 'source_data', 'timesteps_array','results_array','general_results');
+    else
+        error(['file ' data_config.output.active_expt_combined_results_file_path 'already exists!']);
+    end
+    fprintf('done.\n');
 
 end
 
