fb314e630caa207a13884f979b30a7769d6f5884
CommanderPho <PhoHale@gmail.com>
2021-10-26 15:40:22 -0400

    Working spike tables clustered by ripples

--- prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
+++ prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
@@ -10,22 +10,31 @@ active_spike_ripple_indices = cellfun(@(spike_ripple_indicies, is_spike_ripple) 
 [active_spike_flatSpikeTimes, active_spike_flatSpikeUnitIDs] = fnUnitSpikeCells2FlatSpikes(active_spike_times);
 
 
-% Test single cell array input:
-[curr_flattened_table] = fnFlattenCellsToContents(active_processing.spikes.time, active_processing.spikes.isRippleSpike, active_processing.spikes.RippleIndex);
-% Test multiple cell array inputs:
-[curr_flattened_table] = fnFlattenCellsToContents(active_processing.spikes.time, active_processing.spikes.isRippleSpike, active_processing.spikes.RippleIndex);
-
-% Test table input version:
+%% Flatten subset of spikes table for efficient ripple-related processing:
 curr_cell_table = table(active_processing.spikes.time, ...
         active_processing.spikes.isRippleSpike, ...
         active_processing.spikes.RippleIndex, ...
         'VariableNames', {'time','isRippleSpike','RippleIndex'});
 [curr_flattened_table] = fnFlattenCellsToContents(curr_cell_table);
 
+% curr_flattened_table(curr_flattened_table.isRippleSpike)
+
+%% Removes rows with missing values, meaning the rows with RippleIndex == NaN (meaning they aren't ripple spikes)
+curr_ripplesOnly_flattened_table = rmmissing(curr_flattened_table);
+
+%% Filter again to a specific ripple index
+unique_ripple_indices = unique(curr_ripplesOnly_flattened_table.RippleIndex);
+eachRipple_filtered_flattened_table = cell([length(unique_ripple_indices), 1]);
+parfor ripple_idx = 1:length(unique_ripple_indices)
+    eachRipple_filtered_flattened_table{ripple_idx} = curr_ripplesOnly_flattened_table(curr_ripplesOnly_flattened_table.RippleIndex == unique_ripple_indices(ripple_idx), :);
+end
+
+
+
 
 %% fnReconstructCellsFromFlattenedContents
 % Test reconstruction of original test table from the flattened table:
-[reconstructedCellTable] = fnReconstructCellsFromFlattenedContents(curr_flattened_table);
+[reconstructedCellTable] = fnReconstructCellsFromFlattenedContents(curr_ripplesOnly_flattened_table);
 
 
 
