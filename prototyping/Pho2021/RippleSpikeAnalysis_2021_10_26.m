1129949decbd4a23526ad9a3ece39972be479277
CommanderPho <PhoHale@gmail.com>
2021-10-26 13:27:57 -0400

    Working on flattening implementation

--- prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
+++ prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
@@ -13,3 +13,52 @@ active_spike_ripple_indices = cellfun(@(spike_ripple_indicies, is_spike_ripple) 
 % Get uniques across all of them:
 
 unique(active_spike_ripple_unique_indicies
+
+
+
+
+%% accepts a cell array of length N, with each cell containing items with the same datatype but potentially different sizes.
+
+curr_cell_table = table(active_processing.spikes.time, ...
+    active_processing.spikes.isRippleSpike, ...
+    active_processing.spikes.RippleIndex, ...
+'VariableNames', {'time','isRippleSpike','RippleIndex'});
+
+% first_cell_array = active_processing.spikes.time;
+first_cell_array = curr_cell_table{:,1};
+num_cell_arrays_to_flatten = width(curr_cell_table);
+
+num_cells = length(first_cell_array);
+cell_content_counts = cellfun(@length, first_cell_array);
+cell_indicies = 1:num_cells;
+
+% The total number of output items for the flattened array
+flattened_total_item_count = sum(cell_content_counts, 'all');
+
+% flattened_UnitIDs: the unit the original entry belongs to:
+flattened_UnitIDs = repelem(cell_indicies, cell_content_counts); % the second argument specifies how many times to repeat each item in the first array
+
+% Now flatten each table variable:
+
+% curr_cell_table{:,1}
+
+% Closed-form vectorized flattening for non-equal sized cells
+   
+flatTableColumns = cell([num_cell_arrays_to_flatten 1]);
+% flatTableCells = cell([flattened_total_item_count, num_cell_arrays_to_flatten]);
+curr_flattened_table = table();
+% curr_flattened_table = table('Size', [flattened_total_item_count, num_cell_arrays_to_flatten],'VariableTypes', varfun(@class, curr_cell_table, 'OutputFormat', 'cell'));
+
+for var_idx = 1:num_cell_arrays_to_flatten
+%     flatTableCells{var_idx} = [curr_cell_table{:,var_idx}{:}]; %bad 2D cell array implementation
+    flatTableColumns{var_idx} = [curr_cell_table{:,var_idx}{:}];
+    curr_variable_name = curr_cell_table.Properties.VariableNames{var_idx};
+    curr_flattened_table.(curr_variable_name) = flatTableColumns{var_idx}';
+%     curr_flattened_table.(curr_variable_name) = [curr_cell_table{:,var_idx}{:}]';
+end
+
+
+% curr_flattened_table = cell2table(flatTableCells);
+% curr_flattened_table = cell2table(flatTableColumns, ...
+%     'VariableNames', curr_cell_table.Properties.VariableNames);
+
