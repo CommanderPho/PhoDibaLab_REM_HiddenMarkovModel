74c3e0e0e2226a292b27497e834206189b9fbf4c
CommanderPho <PhoHale@gmail.com>
2021-10-26 11:23:48 -0400

    A few updates

--- helpers/Analysis/fnUnitSpikeCells2FlatSpikes.m
+++ helpers/Analysis/fnUnitSpikeCells2FlatSpikes.m
@@ -7,16 +7,30 @@ function [flatSpikeTimes, flatSpikeUnitIDs] = fnUnitSpikeCells2FlatSpikes(unitSp
     if ~exist('includedCellIDs','var')
         % If no IDs provided, generate IDs 1:N for each unit
         includedCellIDs = 1:num_unit_cells;
+        if size(unitSpikeCells) ~= size(includedCellIDs) 
+            % Try the transpose
+            includedCellIDs = includedCellIDs';
+        end
     end
 
     if size(unitSpikeCells) ~= size(includedCellIDs) 
-        error('unitSpikeCells is not the same size as includedCellIDs!')
+        % Try the transpose
+%         unitSpikeCells = unitSpikeCells';
+%         includedCellIDs = includedCellIDs';
+        % If it's still not the correct size, give up
+%         if size(unitSpikeCells) ~= size(includedCellIDs)
+            error('unitSpikeCells is not the same size as includedCellIDs!')
+%         end
     end
     
     flatSpikeTimes = [];
     flatSpikeUnitIDs = [];
+
+    % Closed-form vectorized flattening for non-equal sized cells
+    flatSpikeTimes = [unitSpikeCells{:}];
+
     for i = 1:num_unit_cells
-        flatSpikeTimes = [flatSpikeTimes; unitSpikeCells{i}];
+%         flatSpikeTimes = [flatSpikeTimes; unitSpikeCells{i}]; %Previously used this method to build up the array, but had issues with vertcat 
         flatSpikeUnitIDs = [flatSpikeUnitIDs; repmat(includedCellIDs(i),[length(unitSpikeCells{i}) 1])];
     end
 
--- prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
+++ prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
@@ -3,10 +3,13 @@
 active_spike_times = cellfun(@(spike_times, is_spike_ripple) spike_times(is_spike_ripple), active_processing.spikes.time(plot_outputs.filter_active_units), active_processing.spikes.isRippleSpike(plot_outputs.filter_active_units), 'UniformOutput', false); %% Filtered to only show the ripple spikes
 active_spike_ripple_indices = cellfun(@(spike_ripple_indicies, is_spike_ripple) spike_ripple_indicies(is_spike_ripple), active_processing.spikes.RippleIndex(plot_outputs.filter_active_units), active_processing.spikes.isRippleSpike(plot_outputs.filter_active_units), 'UniformOutput', false); %% Filtered to only show the ripple spikes
 
+% [cnt_unique, unique_a] = hist(a,unique(a));
 
-[cnt_unique, unique_a] = hist(a,unique(a));
+[active_spike_ripple_unique_count, active_spike_ripple_unique_indicies] = cellfun(@(spike_ripple_indicies) hist(spike_ripple_indicies, unique(spike_ripple_indicies)), active_spike_ripple_indices, 'UniformOutput', false); %% Filtered to only show the ripple spikes
+
+[active_spike_flatSpikeTimes, active_spike_flatSpikeUnitIDs] = fnUnitSpikeCells2FlatSpikes(active_spike_times);
 
 
+% Get uniques across all of them:
 
-active_spike_ripple_indices = cellfun(@(spike_ripple_indicies, is_spike_ripple) spike_ripple_indicies(is_spike_ripple), active_processing.spikes.RippleIndex(plot_outputs.filter_active_units), active_processing.spikes.isRippleSpike(plot_outputs.filter_active_units), 'UniformOutput', false); %% Filtered to only show the ripple spikes
-
+unique(active_spike_ripple_unique_indicies
