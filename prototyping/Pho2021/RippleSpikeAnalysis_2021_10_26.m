2e9550aaee34da0b84737ec5573cc222f14b7785
Pho Hale <PhoHale@gmail.com>
2021-10-27 15:55:01 -0400

    comment out unifinished code

--- prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
+++ prototyping/Pho2021/RippleSpikeAnalysis_2021_10_26.m
@@ -87,43 +87,43 @@ target_options.behavioral_states_variable_name = 'behavioral_states';
 % [track_quiet_outputs.reconstructedCellTable] = fnReconstructCellsFromFlattenedContents(track_quiet_outputs.filtered_table);
 
 %%% This is going to be very inefficient
-
-for unit_idx = 1:height(track_quiet_outputs.reconstructedCellTable)
-    
-    % Get the ripple indexes this belongs in:
-    currUnitRippleIndicies = track_quiet_outputs.reconstructedCellTable.rippleRelativeSequenceIndex{unit_idx};
-    
-    for unit_ripple_idx = 1:length(currUnitRippleIndicies)
-        % All the unitIDs within this ripple
-        currUnitRippleUnitIDs = track_quiet_outputs.reconstructedCellTable.eachRipple_filtered_flattened_table{unit_ripple_idx}.flattened_UnitIDs:
-        currUnitRippleUnitRelativeTimeOffsets = track_quiet_outputs.reconstructedCellTable.eachRipple_filtered_flattened_table{unit_ripple_idx}.rippleRelativeTimeOffsets:
-
-        %%% NOT YET FINISHED 2021-10-26
-        error('Not yet implemented')
-        %%% NOT YET FINISHED
-        
-
-        % Find the location of the current unit
-        currUnitFoundIndicies = find(currUnitRippleUnitIDs == unit_idx);
-
-  
-        %% TODO: The computational complexity here is going to take off, I need to find a smarter implementation.
-        % The concept was to find all the units that preceed our current unit_idx (curr_ripple_leading_units)
-        
-        currUnitRippleUnitRelativeTimeOffsets(curr_ripple_leading_units) % get the offsets for the units that lead our unit
-
-        master_weights{unit_idx, curr_ripple_leading_units} = -1 .* currUnitRippleUnitRelativeTimeOffsets(curr_ripple_leading_units);
-
-        %% ?? TODO: Need to set the reciprocal weights too?
-        master_weights{curr_ripple_leading_units, unit_idx} = currUnitRippleUnitRelativeTimeOffsets(curr_ripple_leading_units);
-        
-        currUnitRippleSequenceIndex = track_quiet_outputs.reconstructedCellTable.rippleRelativeSequenceIndex{unit_idx};
-       
-    
-
-    end
-
-end
+% 
+% for unit_idx = 1:height(track_quiet_outputs.reconstructedCellTable)
+%     
+%     % Get the ripple indexes this belongs in:
+%     currUnitRippleIndicies = track_quiet_outputs.reconstructedCellTable.rippleRelativeSequenceIndex{unit_idx};
+%     
+%     for unit_ripple_idx = 1:length(currUnitRippleIndicies)
+%         % All the unitIDs within this ripple
+%         currUnitRippleUnitIDs = track_quiet_outputs.reconstructedCellTable.eachRipple_filtered_flattened_table{unit_ripple_idx}.flattened_UnitIDs:
+%         currUnitRippleUnitRelativeTimeOffsets = track_quiet_outputs.reconstructedCellTable.eachRipple_filtered_flattened_table{unit_ripple_idx}.rippleRelativeTimeOffsets:
+% 
+%         %%% NOT YET FINISHED 2021-10-26
+%         error('Not yet implemented')
+%         %%% NOT YET FINISHED
+%         
+% 
+%         % Find the location of the current unit
+%         currUnitFoundIndicies = find(currUnitRippleUnitIDs == unit_idx);
+% 
+%   
+%         %% TODO: The computational complexity here is going to take off, I need to find a smarter implementation.
+%         % The concept was to find all the units that preceed our current unit_idx (curr_ripple_leading_units)
+%         
+%         currUnitRippleUnitRelativeTimeOffsets(curr_ripple_leading_units) % get the offsets for the units that lead our unit
+% 
+%         master_weights{unit_idx, curr_ripple_leading_units} = -1 .* currUnitRippleUnitRelativeTimeOffsets(curr_ripple_leading_units);
+% 
+%         %% ?? TODO: Need to set the reciprocal weights too?
+%         master_weights{curr_ripple_leading_units, unit_idx} = currUnitRippleUnitRelativeTimeOffsets(curr_ripple_leading_units);
+%         
+%         currUnitRippleSequenceIndex = track_quiet_outputs.reconstructedCellTable.rippleRelativeSequenceIndex{unit_idx};
+%        
+%     
+% 
+%     end
+% 
+% end
 
 
 
