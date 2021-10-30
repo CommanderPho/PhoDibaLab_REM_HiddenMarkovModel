df9cda9feeccd2939428103b340a890bd44e41cf
CommanderPho <PhoHale@gmail.com>
2021-10-30 16:33:16 -0400

    updated class comments

--- DEVELOPMENT/Pho2021/PhoFallAnalysis2021.m
+++ DEVELOPMENT/Pho2021/PhoFallAnalysis2021.m
@@ -1,43 +1,44 @@
 classdef PhoFallAnalysis2021
+
+ %________________________________ 
+% PhoFallAnalysis2021 - One line description of what the function performs
+% Detailed explanation goes here
+% 
+% Author: Pho Hale
+% PhoHale.com 
+% email: halechr@umich.edu
+% Created: 30-Oct-2021 ; Last revision: 30-Oct-2021 
+
+% ------------- BEGIN CODE --------------
+
+
     %PhoFallAnalysis2021 Summary of this class goes here
     %   Detailed explanation goes here
-
     properties
         Property1
     end
-
     methods
         function obj = PhoFallAnalysis2021(inputArg1,inputArg2)
             %UNTITLED8 Construct an instance of this class
             %   Detailed explanation goes here
             obj.Property1 = inputArg1 + inputArg2;
         end
-
         function outputArg = method1(obj,inputArg)
             %METHOD1 Summary of this method goes here
             %   Detailed explanation goes here
             outputArg = obj.Property1 + inputArg;
         end
     end
-
     methods (Static)
-
         
        function [filter_active_units, original_unit_index] = showPlaceCellSpatialTuningsPlot(PlaceFields, PositionBins)
            % Like spatialTunings_biDir
           
-
             [~, ~] = fnPlotPlaceCellSpatialTunings(PlaceFields, 'linearPoscenters', linearPoscenters, 'unitLabels', num2cellstr(plot_outputs.original_unit_index));
-
        end
-
-
-
     end % end static method block
-
     %% Refactored from PhoDiba_BayesianDecoding2021.m
     methods (Static)
-
         function [PositionBins, placeFieldTuningCurves, sortedTuningCurveIndicies] = showPlaceCellSpatialTuningPlots(spatialTunings_biDir, fileinfo, unitIndicies)
             import PhoFallAnalysis2021.*
             %% Needs fileinfo.xyz2 and spatialTunings_biDir to work:
@@ -62,7 +63,6 @@ classdef PhoFallAnalysis2021
             [~, ~] = fnPlotPlaceCellSpatialTunings(placeFieldTuningCurves(sortedTuningCurveIndicies, :),'linearPoscenters', PositionBins, 'unitLabels', num2cellstr(sortedTuningCurveIndicies));
             title('Sorted Spatial Tunings')
         end
-
         function [maxL, likelihood, activeTimeBins] = subfn_computeMaximumLikelihood(spikeTimes, PlaceFields, PositionBins, TrialStart, TrialEnd, tau)
             import PhoFallAnalysis2021.*
             nTimeBins = ceil((TrialEnd-TrialStart)/tau);
@@ -80,7 +80,6 @@ classdef PhoFallAnalysis2021
             [~, index] = max(likelihood, [], 1);
             maxL = PositionBins(index);
         end % end subfn_computeMaximumLikelihood
-
         function [posBinEdges, linearPoscenters, linearPos] = subfn_buildMissingPositionBinInformation(xyt2)
             % xyt2: a cell array of the format that's present in fileinfo.xyt2
             posBinSize = 2; % in cm
@@ -90,7 +89,6 @@ classdef PhoFallAnalysis2021
             posBinEdges = min(linearPos(:, 1)): posBinSize: max(linearPos(:, 1)); % edges of the position bins - 1x109 double
             linearPoscenters = posBinEdges(1:end-1) + posBinSize/2; % 1 x 108 double - center of the position bins
         end
-
         function subfn_plotTrajectoryComparison(activeTimeBins, maxL, animalPositionTimestamps, animalPosition)
             % subfn_plotTrajectoryComparison(activeTimeBins, maxL, t_rel, fileinfo);
             figure(1)
@@ -109,7 +107,6 @@ classdef PhoFallAnalysis2021
         %     xlim([active_processing.behavioral_epochs.start_seconds(2) 0.1 * active_processing.behavioral_epochs.end_seconds(2)]); % [15141, 15162] are good
             title('most-likely vs. observed trajectory comparsion');
         end
-
         function subfn_plotSampleTrajectories(PositionBins, likelihood)
             % subfn_plotSampleTrajectories: display the likely trajectory for
             % several time bins
@@ -129,8 +126,8 @@ classdef PhoFallAnalysis2021
             end
             title('most-likely sample trajectories')
         end % end subfn_plotSampleTrajectories
-
     end % end 
-
 end %% end static methods block
 
+
+% ------------- END OF CODE --------------
